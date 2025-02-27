9736ec02b7b7f11e3c3630640cba816d212014d3
regsusan <103440541+regsusan@users.noreply.github.com>
2025-02-27 11:06:59 +0400

    Modified School.java

--- model/School.java
+++ model/School.java
@@ -1,5 +1,46 @@
 package model;
+package model;
+
+import java.util.ArrayList;
 
 public class School {
-    
-}
+    private String schoolName;
+    private boolean isResearchCenter;
+    private int yearOfEstablishment;
+    private String majors;
+    private String notableAlumni;
+
+public School(String school, boolean researchcenter, int yearOfEstablish, String majors, String notableAlumni){
+    this.schoolName = schoolName;
+    this.isResearchCenter = isResearchCenter;
+    this.yearOfEstablishment = yearOfEstablishment;
+    this.majors = majors;
+    this.notableAlumni = notableAlumni;}
+
+    public String getName() { return schoolName; }
+    public void setName(String name) { this.schoolName = name; }
+
+    public boolean isResearchCenter() { return isResearchCenter; }
+    public void setResearchCenter(boolean isResearchCenter) { this.isResearchCenter = isResearchCenter; }
+
+    public int getYearOfEstablishment() { return yearOfEstablishment; }
+    public void setYearOfEstablishment(int yearOfEstablishment) { this.yearOfEstablishment = yearOfEstablishment; }
+
+    public String getMajors() { return majors; }
+    public void setMajors(String majors) { this.majors = majors; }
+
+    public String getNotableAlumni() { return notableAlumni; }
+    public void setNotableAlumni(String notableAlumni) { this.notableAlumni = notableAlumni; }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "name='" + schoolName + '\'' +
+                ", isResearchCenter=" + isResearchCenter +
+                ", yearOfEstablishment=" + yearOfEstablishment +
+                ", majors='" + majors + '\'' +
+                ", notableAlumni='" + notableAlumni + '\'' +
+                '}';
+    }}
+
+
